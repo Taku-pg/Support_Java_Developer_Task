1854d6bc7ae02d7ac91242ad3dd2e28dc1f93c93
Taku-pg <kaitsu505@gmail.com>
2026-04-14 13:58:27 +0200

    Initial commit

---
+++ src/main/java/com/example/support_java_devloper_task/SupportJavaDevloperTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.support_java_devloper_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportJavaDevloperTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportJavaDevloperTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/support_java_devloper_task/SupportJavaDevloperTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.support_java_devloper_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportJavaDevloperTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
