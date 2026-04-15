0ca6c694b28bfac24c951468a7d960199681611b
Taku-pg <kaitsu505@gmail.com>
2026-04-15 18:14:51 +0200

    UPDATE: remove unnecessary method

--- src/main/java/com/example/support_java_devloper_task/Repository/ProducerRepository.java
+++ src/main/java/com/example/support_java_devloper_task/Repository/ProducerRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProducerRepository extends JpaRepository<Producer,Long> {
-    Optional<Producer> getProducerByProducerName(String producerName);
-
     Optional<Producer> findProducerById(Long id);
 }
